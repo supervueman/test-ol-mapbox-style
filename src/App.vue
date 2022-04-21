9776026385ae9d711abfa83f60b6acdc224f5c65
supervueman <supervueman@gmail.com>
2022-04-21 10:37:58 +0400

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {applyStyle} from 'ol-mapbox-style';
+import VectorTileLayer from 'ol/layer/VectorTile.js'
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  mounted() {
+    const map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'map',
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+
+    const layer = new VectorTileLayer({declutter: true});
+    applyStyle(layer, process.env.VUE_APP_LAYER_URL, {accessToken: process.env.VUE_APP_MAPBOX_TOKEN});
+
+    map.addLayer(layer)
+  },
 }
 </script>
 
 <style>
+* {
+  box-shadow: border-box;
+}
+body {
+  margin: 0;
+}
+#map {
+  width: 100vw;
+  height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
